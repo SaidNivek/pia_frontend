26e13aa4ec27f5288869f255b8363d004b5a5bfd
SaidNivek <kevin.vi.dias@gmail.com>
2024-02-18 21:03:18 -0500

    Stub message board container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './containers/Home';
+import MessageBoard from './containers/MessageBoard';
 import Calendar from './containers/Calendar';
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/calendar" element={<Calendar />} />
+        <Route path="/messagebaord" element={<MessageBoard />} />
       </Routes>
       <Footer />
     </>
